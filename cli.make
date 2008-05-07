72bc753b18666e5c19ea1d974a1e1ca4b3d60ea0
Mirco Bauer <meebey@debian.org>
2008-05-07 20:06:11 +0000

    dh sequence support

--- cli.make
+++ cli.make
@@ -1,1 +1,3 @@
-export MONO_SHARED_DIR=$(CURDIR)
+#export MONO_SHARED_DIR=$(CURDIR)
+export MONO_DISABLE_SHM = 1
+export DH_OPTIONS += --with=cli
---
+++ cli.pm
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+# debhelper sequence file for cli-common-dev scripts
+
+use warnings;
+use strict;
+use Debian::Debhelper::Dh_Lib;
+
+insert_after("dh_fixperms", "dh_clifixperms");
+
+# have to use dh_installdeb as those scripts need to run for
+# binary-arch and binary-indep packages
+insert_before("dh_installdeb", "dh_clistrip");
+insert_before("dh_installdeb", "dh_cligacpolicy");
+insert_before("dh_installdeb", "dh_makeclilibs");
+insert_before("dh_installdeb", "dh_installcligac");
+insert_before("dh_installdeb", "dh_clideps");
+
+1;
